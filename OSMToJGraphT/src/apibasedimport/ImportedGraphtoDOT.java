4918d7013df26c2e59513096dde5ccebe4687bf0
Roopak Sinha <Roopak Sinha>
2019-09-05 15:51:39 +1200

    fixed JgraphT error

--- OSMToJGraphT/src/apibasedimport/ImportedGraphtoDOT.java
+++ OSMToJGraphT/src/apibasedimport/ImportedGraphtoDOT.java
@@ -8,18 +8,19 @@ import org.jgrapht.Graph;
 import org.jgrapht.io.ComponentNameProvider;
 import org.jgrapht.io.DOTExporter;
 import org.jgrapht.alg.shortestpath.*;
+import org.jgrapht.graph.DefaultEdge;
 
 public class ImportedGraphtoDOT {
 	
-	public static void exportOSMGraphToFile(Graph<ImportedNode, ImportedEdge> g, String fileName) {
+	public static void exportOSMGraphToFile(Graph<Long, ImportedEdge> g, String fileName, ParsingMapDataHandler nodesMapProvider) {
 		try{
 			File outputFile = new File(fileName);
 			if(!outputFile.exists())
 				outputFile.createNewFile();
-			ElementNameProvider objectToStringProvider = new ElementNameProvider();
+			ElementNameProvider objectToStringProvider = new ElementNameProvider(nodesMapProvider);
 			//EdgeLabelProvider eProvider = new EdgeLabelProvider();
 			FileWriter fileWriter = new FileWriter(outputFile);
-			DOTExporter<ImportedNode, ImportedEdge> dotExporter = new DOTExporter<ImportedNode, ImportedEdge>(objectToStringProvider, 
+			DOTExporter<Long, ImportedEdge> dotExporter = new DOTExporter<Long, ImportedEdge>(objectToStringProvider, 
 					objectToStringProvider,  
 					objectToStringProvider);
 			dotExporter.exportGraph(g, fileWriter);
--- OSMToJGraphT/src/apibasedimport/ParsingMapDataHandler.java
+++ OSMToJGraphT/src/apibasedimport/ParsingMapDataHandler.java
@@ -1,11 +1,13 @@
 package apibasedimport;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DirectedPseudograph;
 import org.jgrapht.graph.SimpleWeightedGraph;
 
+import de.westnordost.osmapi.map.MapDataDao;
 import de.westnordost.osmapi.map.data.BoundingBox;
 import de.westnordost.osmapi.map.data.Node;
 import de.westnordost.osmapi.map.data.Relation;
@@ -17,8 +19,15 @@ import de.westnordost.osmapi.map.handler.MapDataHandler;
 
 public class ParsingMapDataHandler extends DefaultMapDataHandler implements MapDataHandler {
 
-	private Graph<ImportedNode, ImportedEdge> g = new SimpleWeightedGraph<>(ImportedEdge.class);
-	private HashMap<Long,ImportedNode> myNodes = new HashMap<Long, ImportedNode>();
+	
+	private Graph<Long, ImportedEdge> g = new DirectedPseudograph<>(ImportedEdge.class);
+	private HashMap<Long,Node> myNodes = new HashMap<Long, Node>();
+	
+	
+	public  HashMap<Long, Node> getNodesMap() {
+		return this.myNodes;
+	}
+	
 	
 	@Override
 	public void handle(BoundingBox bounds) {
@@ -28,32 +37,45 @@ public class ParsingMapDataHandler extends DefaultMapDataHandler implements MapD
 
 	@Override
 	public void handle(Node node) {
-		
-		
+
 		/*if(node.getTags()!=null) {
 			System.out.println("Got tagged Node: "+node+" with ID:"+node.getId());
 			for(String tag: node.getTags().keySet())
 			System.out.println("Tag:"+tag+",value:"+node.getTags().get(tag));
 		}*/
 		super.handle(node);
-		ImportedNode n = new ImportedNode(node);
-		g.addVertex(n);
-		myNodes.put(node.getId(), n);
+		//ImportedNode n = new ImportedNode(node.getId());
+		g.addVertex(node.getId());
+		myNodes.put(node.getId(), node);
 		
 		
 	}
 
 	@Override
 	public void handle(Way way) {
-		System.out.println("Got Way: "+way+" with ID:"+way.getId());
 		super.handle(way);
+		System.out.println("Got Way: "+way+" with ID:"+way.getId());
 		long previousNodeID = -1;
 		for(long nodeID: way.getNodeIds()) {
 			if(previousNodeID==-1) {
 				previousNodeID = nodeID;
 				continue;
 			}	 
-			g.addEdge(myNodes.get(previousNodeID), myNodes.get(nodeID));
+			if(!myNodes.containsKey(previousNodeID)) {
+				System.out.println("previously unencountered node found in a way");
+				myNodes.put(previousNodeID, null);
+			}
+			if(!myNodes.containsKey(nodeID)) {
+				System.out.println("previously unencountered node found in a way");
+				myNodes.put(nodeID, null);
+			}
+			System.out.println("Adding edge from node ID:"+previousNodeID+" to:"+nodeID);
+			ImportedEdge edge = new ImportedEdge(previousNodeID, nodeID);
+			g.addEdge(previousNodeID, nodeID, edge);
+			//System.out.println("Node 1 is:"+myNodes.get(previousNodeID));
+			//System.out.println("Node 2 is:"+myNodes.get(nodeID));
+			//System.out.println("Adding edge from node:"+myNodes.get(previousNodeID)+" to:"+myNodes.get(nodeID));
+
 			previousNodeID = nodeID;
 		}
 		
@@ -66,7 +88,7 @@ public class ParsingMapDataHandler extends DefaultMapDataHandler implements MapD
 		super.handle(relation);
 	}
 	
-	public Graph<ImportedNode, ImportedEdge> getGraph() {
+	public Graph<Long, ImportedEdge> getGraph() {
 		return g;
 	}
 
